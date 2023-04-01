0c71f2d434f12b2e4e92855cb576a94020f54e2b
inmar salinas <inmarsalinas@yahoo.com>
2023-03-31 19:54:13 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = 2 * 31;
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) +1;
+Math.random();
+
+let num3 = 4;
+let num4 = 8;
+let mod = 4 % 8;
+console.log(mod);
+
+let min = 0;
+let max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
